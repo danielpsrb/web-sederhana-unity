304cf3ff04430705f0e952f795736faac84b5014
danielpsrb <danielpsrb01@gmail.com>
2024-04-30 21:33:41 +0700

    menambahkan component baru utk daftar anime

---
+++ src/components/DaftarAnime/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function DaftarAnime() {
+    return (
+        <div className='grid grid-cols-3 gap-2 h-32'>
+            <div className='bg-indigo-400'>
+                <img src='https://placehold.co/600x400/png' alt='images' />
+            </div>
+            <div className='bg-indigo-400'>
+                <img src='https://placehold.co/600x400/png' alt='images' />
+            </div>
+            <div className='bg-indigo-400'>
+                <img src='https://placehold.co/600x400/png' alt='images' />
+            </div>
+            <div className='bg-indigo-400'>
+                <img src='https://placehold.co/600x400/png' alt='images' />
+            </div>
+            <div className='bg-indigo-400'>
+                <img src='https://placehold.co/600x400/png' alt='images' />
+            </div>
+            <div className='bg-indigo-400'>
+                <img src='https://placehold.co/600x400/png' alt='images' />
+            </div>
+        </div>
+    )
+}
+
+export default DaftarAnime
