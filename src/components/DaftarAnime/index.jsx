de9839ae720e24fbf8b1bf5c58b5e315c16314b3
danielpsrb <danielpsrb01@gmail.com>
2024-04-30 23:22:22 +0700

    fetch data anime dan memunculkan daftar anime

--- src/components/DaftarAnime/index.jsx
+++ src/components/DaftarAnime/index.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-function DaftarAnime() {
+const DaftarAnime = ({ title, images }) => {
+    const [anime, setAnime] = useState(null);
+
+    useEffect(() => {
+        const fetchAnime = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_REACT_API_URL}/top/anime`);
+                const data = await response.json();
+                setAnime(data);
+                console.log(data);
+            } catch (error) {
+                console.error('error fetchig data:', error);
+            }
+        };
+        fetchAnime();
+    }, []);
+
     return (
-        <div className='grid grid-cols-3 gap-2 h-32'>
-            <div className='bg-indigo-400'>
-                <img src='https://placehold.co/600x400/png' alt='images' />
-            </div>
-            <div className='bg-indigo-400'>
-                <img src='https://placehold.co/600x400/png' alt='images' />
-            </div>
-            <div className='bg-indigo-400'>
-                <img src='https://placehold.co/600x400/png' alt='images' />
-            </div>
-            <div className='bg-indigo-400'>
-                <img src='https://placehold.co/600x400/png' alt='images' />
-            </div>
-            <div className='bg-indigo-400'>
-                <img src='https://placehold.co/600x400/png' alt='images' />
-            </div>
-            <div className='bg-indigo-400'>
-                <img src='https://placehold.co/600x400/png' alt='images' />
+        <div className=''>
+            <div className='grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4'>
+            {anime && anime.data && anime.data.map(data => {
+                return (
+                    <div key={data.mal_id} className='shadow-xl '>
+                        <img src={data.images.webp.image_url} alt="images" width={350} height={350} loading='lazy' />
+                        <h3 className='font-bold md:text-xl text-md p-4'>{data.title}</h3>
+                    </div>
+                );
+            })}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default DaftarAnime
+export default DaftarAnime;
