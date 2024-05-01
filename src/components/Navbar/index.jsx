9bf834d1dd3f4155dee027b3bde7d94e68a1466c
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 11:52:12 +0700

    membuat agar tombol light/dark mode berfungsi

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    const [theme, setTheme] = useState('system')
+    const [theme, setTheme] = useState('system');
+    const element = document.documentElement;
 
     const options = [
         {
@@ -17,7 +18,20 @@ const Navbar = () => {
             icon: "settings-outline",
             text: "system",
         }
-    ]
+    ];
+
+    useEffect(() => {
+        switch (theme) {
+            case 'dark':
+                element.classList.add('dark');
+                break;
+            case 'light':
+                element.classList.remove('dark');
+                break;
+            default:
+                break;
+        }
+    }, [theme])
 
     return (
         <header className='bg-indigo-400'>
@@ -27,9 +41,12 @@ const Navbar = () => {
                     {options?.map(opt=>(
                         <button 
                             key={opt.text} 
+                            onClick={()=>setTheme(opt.text)}
                             className={`w-8 h-8 leading-9 text-xl rounded-md m-1 mr-4 ${theme === opt.text && 'text-sky-500'} bg-slate-700`}
                         >
-                            <ion-icon name={opt.icon}></ion-icon>
+                            <ion-icon 
+                                name={opt.icon}
+                            ></ion-icon>
                         </button>
                     ))}
                     <input placeholder='cari anime..' className='w-80 md:w-auto p-2 pr-10' />
