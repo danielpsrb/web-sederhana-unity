ea5de445d39c24271144d6ffd0c80fd49454a01b
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 19:09:38 +0700

    update nav

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
     return (
         <header className='bg-indigo-400 dark:bg-[#FE8040]'>
             <div className='flex lg:flex-row flex-col justify-between p-4 items-center'>
-                <Link to='/' className='font-bold text-2xl text[#cccccc]'>DANS ANIME</Link>
+                <Link to='/' className='font-bold text-2xl text[#cccccc]'>RyotaAnime</Link>
                 <div className='flex items-center flex-col lg:flex-row'>
                     <InputSearchAnime />
                     <div className="lg:flex items-center">
