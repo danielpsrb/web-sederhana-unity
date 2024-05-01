c313dcbd42c4c457be7769b4f39ea7f3e55028a0
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 20:01:03 +0700

    validasi input search

--- src/components/Navbar/InputSearchAnime.jsx
+++ src/components/Navbar/InputSearchAnime.jsx
@@ -1,16 +1,28 @@
-import React, { useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const InputSearchAnime = () => {
-
     const searchRef = useRef();
     const navigate = useNavigate();
+    const location = useLocation();
+    const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+        if (location.pathname === '/') {
+            setSearchValue('');
+        }
+    }, [location]);
 
     const handleSearchInput = (e) => {
-        e.preventDefault();
-        const keyword = searchRef.current.value;
-        navigate(`/search/${keyword}`);
-    }
+        if (e.key === "Enter" || e.type === "click") {
+            e.preventDefault();
+            const keyword = searchRef.current.value;
+    
+            if (keyword.trim() !== "") {
+                navigate(`/search/${keyword}`);
+            }
+        }
+    };
 
     return (
         <div className='relative'>
@@ -18,12 +30,15 @@ const InputSearchAnime = () => {
                 placeholder='cari anime..' 
                 className='w-80 md:w-auto p-2 pr-10 text-black rounded-md' 
                 ref={searchRef}
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                onKeyDown={handleSearchInput}
             />
             <button className='absolute top-2.5 end-2 text-black text-xl' onClick={handleSearchInput}>
                 <ion-icon name="search-outline"></ion-icon>
             </button>
         </div>
-    )
-}
+    );
+};
 
-export default InputSearchAnime
+export default InputSearchAnime;
