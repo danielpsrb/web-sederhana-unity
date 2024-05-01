fc2f06ed33baa00c4ea07672a44ad958ea5fa8e5
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 12:10:51 +0700

    updated dengan menambahkan localStorage pada thema

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,8 +2,11 @@ import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    const [theme, setTheme] = useState('system');
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") ? localStorage.getItem("theme") : "system"
+    );
     const element = document.documentElement;
+    const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
     const options = [
         {
@@ -20,21 +23,35 @@ const Navbar = () => {
         }
     ];
 
+    function onWindowMatch() {
+        if (localStorage.theme === 'dark' || (!("theme" in localStorage) && darkQuery.matches) 
+        ) {
+            element.classList.add('dark');
+        } else {
+            element.classList.remove('dark');
+        }
+    }
+    onWindowMatch();
+
     useEffect(() => {
         switch (theme) {
             case 'dark':
                 element.classList.add('dark');
+                localStorage.setItem('theme', 'dark');
                 break;
             case 'light':
                 element.classList.remove('dark');
+                localStorage.setItem('theme', 'light');
                 break;
             default:
+                localStorage.removeItem('theme');
+                onWindowMatch();
                 break;
         }
     }, [theme])
 
     return (
-        <header className='bg-indigo-400'>
+        <header className='bg-indigo-400 dark:bg-red-500'>
             <div className='flex md:flex-row flex-col justify-between p-4 items-center'>
                 <Link to='/' className='font-bold text-2xl text-white'>DANS ANIME</Link>
                 <div className='flex items-center'>
