0285170a4ef8279142a837a1fd3cf39b596a13ae
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 15:19:32 +0700

    update Header menerima parameter title, linkTitle, dan linkHref

--- src/components/DaftarAnime/Header.jsx
+++ src/components/DaftarAnime/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Header = () => {
+const Header = ({ title, linkTitle, linkHref }) => {
     return (
         <div className='flex justify-between items-center p-4'>
-            <h1 className='md:text-xl font-medium'>Paling Populer</h1>
-            <Link to='/populer' className='md:text-xl text-md'>Lihat Semua Anime</Link>
+            <h1 className='md:text-2xl font-medium'>{title}</h1>
+            {
+                linkHref && linkTitle
+                ?
+                <Link to={linkHref} className='md:text-xl text-md'>{linkTitle}</Link>
+                :
+                null
+            }
         </div>
     )
 }
