910e6a86e3949794977f4f16ed0f4ffb1f3fff99
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 15:20:29 +0700

    tampilkan data manga mulai dari gambar, dan title

--- src/components/DaftarManga/index.jsx
+++ src/components/DaftarManga/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const DaftarManga = () => {
+const DaftarManga = ({ api }) => {
     return (
-        <div>Daftar Top Manga</div>
+        <div className='grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4'>
+            {api && api.data && api.data.map((manga) => {
+                return (
+                    <div key={manga.mal_id} className='shadow-xl '>
+                        <Link to={`/anime/${manga.mal_id}`}>
+                            <img src={manga.images.webp.image_url} alt="images" width={350} height={350} />
+                            <h3 className='font-bold md:text-xl text-md p-4'>{manga.title}</h3>
+                        </Link>
+                    </div>
+                );
+            })}
+        </div>
     )
 }
 
