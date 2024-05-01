1ba3a01b2ec62c0913590f342af9002bb6aa05ee
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 14:07:22 +0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import DaftarAnime from './components/DaftarAnime'
 import Header from './components/DaftarAnime/Header';
@@ -6,12 +6,27 @@ import Navbar from './components/Navbar';
 
 export default function App() {
 
+  const [animeData, setAnimeData] = useState("");
+
+  useEffect(() => {
+        const fetchAnime = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_REACT_API_URL}/top/anime?limit=8`);
+                const topAnime = await response.json();
+                setAnimeData(topAnime);
+            } catch (error) {
+                console.error('error fetchig data:', error);
+            }
+        };
+        fetchAnime();
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="dark:text-gray-100 dark:bg-slate-900 duration-100">
         <Navbar />
         <Header />
-        <DaftarAnime />
+        <DaftarAnime api={animeData} />
       </div>
     </BrowserRouter>
   )
