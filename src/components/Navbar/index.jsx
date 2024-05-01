4bbb8734795870e8475baa99109dd32402797c77
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 11:37:34 +0700

    menambahkan icon light/dark mode

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const [theme, setTheme] = useState('system')
+
+    const options = [
+        {
+            icon: "sunny",
+            text: "light",
+        },
+        {
+            icon: "moon",
+            text: "dark",
+        },
+        {
+            icon: "settings-outline",
+            text: "system",
+        }
+    ]
+
     return (
         <header className='bg-indigo-400'>
             <div className='flex md:flex-row flex-col justify-between p-4 items-center'>
                 <Link to='/' className='font-bold text-2xl text-white'>DANS ANIME</Link>
-                <input placeholder='cari anime..' className='w-80 md:w-auto p-2 pr-10' />
+                <div className='flex items-center'>
+                    {options?.map(opt=>(
+                        <button 
+                            key={opt.text} 
+                            className={`w-8 h-8 leading-9 text-xl rounded-md m-1 mr-4 ${theme === opt.text && 'text-sky-500'} bg-slate-700`}
+                        >
+                            <ion-icon name={opt.icon}></ion-icon>
+                        </button>
+                    ))}
+                    <input placeholder='cari anime..' className='w-80 md:w-auto p-2 pr-10' />
+                </div>
             </div>
         </header>
     )
