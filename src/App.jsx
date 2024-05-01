26ac9eef68c64a9b27bab163e5e67f9d4ecd2dcd
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 10:58:36 +0700

    menambahkan component header ke halaman root

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import DaftarAnime from './components/DaftarAnime'
+import Header from './components/DaftarAnime/Header';
 import Navbar from './components/Navbar';
 
 export default function App() {
@@ -8,10 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <div className='flex justify-between items-center p-4'>
-        <h1 className='md:text-xl font-medium'>Paling Populer</h1>
-        <Link to='/populer' className='md:text-xl text-md'>Lihat Semua Anime</Link>
-      </div>
+      <Header />
       <DaftarAnime />
     </BrowserRouter>
   )
