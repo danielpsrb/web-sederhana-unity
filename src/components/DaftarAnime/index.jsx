162801a303fb81e81f422817cd38210c620db5e5
danielpsrb <danielpsrb01@gmail.com>
2024-05-01 19:10:06 +0700

    update daftar anime

--- src/components/DaftarAnime/index.jsx
+++ src/components/DaftarAnime/index.jsx
@@ -8,10 +8,10 @@ const DaftarAnime = ({ api }) => {
         <div className='grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4'>
             {api && api.data && api.data.map((animes) => {
                 return (
-                    <div key={animes.mal_id} className='bg-[#ADD8E6] dark:bg-[#345DAC] cards'>
+                    <div key={animes.mal_id} className='bg-[#ADD8E6] hover:bg-[#DCDCDC] dark:bg-[#345DAC] cards'>
                         <Link to={`/anime/${animes.mal_id}`} className='dark:hover:text-yellow-500 transition-all' >
                             <img src={animes.images.webp.image_url} alt="images" width={350} height={350} loading='eager' />
-                            <h3 className='font-bold md:text-xl text-md p-4'>{animes.title}</h3>
+                            <h3 className='font-semibold md:text-xl text-md p-4 hover:text-[#D2691E]'>{animes.title}</h3>
                         </Link>
                     </div>
                 );
