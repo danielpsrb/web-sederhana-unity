3279c1e9323295ee18641eb0e5d6cd474a93b574
danielpsrb <danielpsrb01@gmail.com>
2024-05-02 21:31:48 +0700

    update input search components

--- src/components/Navbar/InputSearchAnime.jsx
+++ src/components/Navbar/InputSearchAnime.jsx
@@ -28,13 +28,13 @@ const InputSearchAnime = () => {
         <div className='relative'>
             <input 
                 placeholder='cari anime..' 
-                className='w-80 md:w-auto p-2 pr-10 text-black rounded-md' 
+                className='w-80 md:w-auto p-2 pr-10 text-gray-800 placeholder-gray-500 rounded-md bg-gray-200 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#ffc639] dark:focus:ring-blue-500' 
                 ref={searchRef}
                 value={searchValue}
                 onChange={(e) => setSearchValue(e.target.value)}
                 onKeyDown={handleSearchInput}
             />
-            <button className='absolute top-2.5 end-2 text-black text-xl' onClick={handleSearchInput}>
+            <button className='absolute top-2.5 end-2 text-black dark:text-white text-xl' onClick={handleSearchInput}>
                 <ion-icon name="search-outline"></ion-icon>
             </button>
         </div>
