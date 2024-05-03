7db9112b7a54d90950e281690c80fc9b2a47b4ac
danielpsrb <danielpsrb01@gmail.com>
2024-05-03 11:21:16 +0700

    add key props index at daftar anime

--- src/components/DaftarAnime/index.jsx
+++ src/components/DaftarAnime/index.jsx
@@ -6,9 +6,9 @@ const DaftarAnime = ({ api }) => {
 
     return (
         <div className='grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4'>
-            {api && api.data && api.data.map((animes) => {
+            {api && api.data && api.data.map((animes, index) => {
                 return (
-                    <div key={animes.mal_id} className='bg-[#ADD8E6] hover:bg-[#DCDCDC] dark:bg-[#345DAC] cards'>
+                    <div key={index} className='bg-[#ADD8E6] hover:bg-[#DCDCDC] dark:bg-[#345DAC] cards'>
                         <Link to={`/anime/${animes.mal_id}`} className='dark:hover:text-yellow-500 transition-all' >
                             <img src={animes.images.webp.image_url} alt="images" width={350} height={350} loading='eager' />
                             <h3 className='font-semibold md:text-xl text-md p-4 hover:text-[#D2691E]'>{animes.title}</h3>
